3999d1a51a00b4b7460a2995e529402d12d80203
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-07-19 23:24:03 -0800

    Remove instructions prompt from screen when ok is pressed

---
+++ script.js
@@ -0,0 +1,4 @@
+let okbutton = document.getElementById('okbutton');
+let instructions = document.getElementById('instructions');
+
+okbutton.addEventListener('click', ()=>instructions.style.display = 'none');
