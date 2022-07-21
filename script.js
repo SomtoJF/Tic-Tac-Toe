a57f3932a966d09dbcc8825cf90e0d079a7dd8f0
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-07-21 02:15:33 -0800

    Check for winning patterns on gameboard

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let player = function (name, marker){
 let player1 = player('Player 1', 'X');
 let player2 = player('Player 2', 'O');
 
-void function(){
+let play = (function(){
     let roundCount = 1;
     
     okbutton.addEventListener('click', ()=>instructions.style.display = 'none');
@@ -21,25 +21,58 @@ void function(){
         };
     })
 
+    let checkWinner = ()=>{
+        const combinations = function (){
+            
+            for(let i = 0; i < gameboard.length; i++){
+                if(gameboard[i].textContent != ""){
+                    if(gameboard[i].textContent == gameboard[i+1].textContent && gameboard[i].textContent == gameboard[i+2].textContent){
+                        if(i != 2 && i != 5){
+                            alert('game end');
+                        };
+                    }
+                    else if(gameboard[i].textContent == gameboard[i+3].textContent && gameboard[i].textContent == gameboard[i+6].textContent){
+                        if(i < 3){
+                            alert('game end');
+                        };
+                    }
+                    else if(gameboard[i].textContent == gameboard[i+4].textContent && gameboard[i].textContent == gameboard[i+8].textContent){
+                        if(i == 0){
+                            alert('game end');
+                        };
+                    }
+                    else if(gameboard[i].textContent == gameboard[i+2].textContent && gameboard[i].textContent == gameboard[i+4].textContent){
+                        if(i == 2){
+                            alert('game end');
+                        };
+                    }
+                }
+                else{
+                    continue;
+                };
+            }
+        };
+        combinations();
+    };
+
     for(let i = 0; i < gameboard.length; i++){
         gameboard[i].addEventListener('click', ()=>{
             if(roundCount%2 == 0){
                 if(gameboard[i].textContent == ''){
                     gameboard[i].textContent = player2.marker;
+                    checkWinner();
                     roundCount++;
                 };
             }
             else{
                 if(gameboard[i].textContent == ''){
                     gameboard[i].textContent = player1.marker;
+                    checkWinner();
                     roundCount++;
                 };
             };
             
         })
     };
-}();
+})();
 
-let play = (function(){
-    
-})()
